5a19bd2be5f804067bc08e1981977efcfb3655f3
Juan Avila <juan.avila@aldeamo.com>
2022-09-19 18:24:55 -0500

    carga de los archivos del proyecto

---
+++ proyecto_semana4/src/main/java/com/proyecto4/proyecto_semana4/ProyectoSemana4Application.java
@@ -0,0 +1,13 @@
+package com.proyecto4.proyecto_semana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemana4Application.class, args);
+	}
+
+}
---
+++ proyecto_semana4/src/test/java/com/proyecto4/proyecto_semana4/ProyectoSemana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto4.proyecto_semana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemana4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
